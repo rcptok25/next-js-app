d6b632255b5a24cff2d034cc403ca4588ce3cf6d
rcptok25 <rcptok25@gmail.com>
2022-08-29 02:36:04 +0300

    initial commit.

---
+++ .history/my-project/pages/index_20220829013752.js
@@ -0,0 +1,52 @@
+import {useState,useEffect} from 'react'
+import Login from './Login'
+import Register from './Register'
+
+import axios from './apiConfig/axios'
+
+
+
+export default function Home() {
+const  [loginPage, setLoginPage] = useState();
+const  [registerPage, setRegisterPage] = useState();
+
+
+  useEffect(() => {
+    let indexPage="";
+    
+    axios.get(`usersControl.php`).then(response => {
+      indexPage=JSON.stringify(response.data.indexPage);
+      
+      if(indexPage==="true"){
+     
+        setLoginPage(true)
+        setRegisterPage(false)
+      }
+      else{
+        
+       setLoginPage(false)
+       setRegisterPage(true)
+      }
+       
+    
+     
+    });
+
+
+  
+
+  })
+
+  
+  return (
+   <>
+  
+   <div style={{display: loginPage ? "block" : "none"}}>  <Login/> </div>
+   <div style={{display: registerPage ? "block" : "none"}}>  <Register/></div>
+ 
+   
+   
+       </>
+
+  )
+}
